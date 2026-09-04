1608c5a7e92362ce7b00c653c978e4afd93485eb
Areeb Hassan <areebhs025@gmail.com>
2026-09-04 18:34:51 +0500

    latest code :v2

--- Backend/portfoliobackend/Terraform/main.tf
+++ Backend/portfoliobackend/Terraform/main.tf
@@ -5,17 +5,25 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
 }
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vm_name}-vnet"
@@ -31,6 +39,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-pip"
   location            = azurerm_resource_group.rg.location
@@ -38,7 +47,6 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Dynamic"
 }
 
-# 4. Network Security Group (Firewall Rules: SSH, HTTP, HTTPS)
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.vm_name}-nsg"
   location            = azurerm_resource_group.rg.location
@@ -94,26 +102,37 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# 6. Associate NSG to Network Interface
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# 7. Ubuntu Linux Virtual Machine
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/devops_key.pem"
+  file_permission = "0600"
+}
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.vm_size
   admin_username      = var.admin_username
+
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file(var.ssh_public_key_path)
+    public_key = tls_private_key.ssh_key.public_key_openssh
   }
 
   os_disk {
